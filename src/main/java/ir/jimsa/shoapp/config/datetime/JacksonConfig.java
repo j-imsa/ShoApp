67a41cfda14f191d72886452c5dca65d28459666
Iman Salehi <cse.isalehi@gmail.com>
2024-10-16 18:15:53 +0330

    add date format

---
+++ src/main/java/ir/jimsa/shoapp/config/datetime/JacksonConfig.java
@@ -0,0 +1,29 @@
+package ir.jimsa.shoapp.config.datetime;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import static ir.jimsa.shoapp.utility.constant.ProjectConstants.DATE_FORMAT;
+import static ir.jimsa.shoapp.utility.constant.ProjectConstants.TIME_FORMAT;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(DATE_FORMAT)));
+        javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(TIME_FORMAT)));
+        objectMapper.registerModule(javaTimeModule);
+        return objectMapper;
+    }
+}
